71e07c307ff0d05cd58572e89617a0fed8b03c9d
IronClawTrem <louie.nutman@gmail.com>
2020-04-02 17:06:20 +0100

    improve wallwalk a bit more

--- src/game/bg_pmove.c
+++ src/game/bg_pmove.c
@@ -1699,6 +1699,69 @@ static void PM_GroundTraceMissed( void )
   pml.walking = qfalse;
 }
 
+/*
+=============
+PM_Wall_Climb_Upside_Down_Drop
+=============
+*/
+static void PM_Wall_Climb_Upside_Down_Drop(void) {
+  if((pm->ps->eFlags & EF_WALLCLIMBCEILING) || pm->ps->grapplePoint[2] < 0)
+  {
+    vec3_t  forward, right, rotated, angles;
+    float   angle_correction = 180.0f;
+
+    AngleVectors( pm->ps->viewangles, forward, right, NULL );
+
+    if(!(pm->ps->eFlags & EF_WALLCLIMBCEILING)) {
+      vec3_t  level_forward, level_right, projected_forward, projected_right;
+      vec3_t  refNormal = { 0.0f, 0.0f, 1.0f };
+      float   projected_forward_dist, projected_right_dist;
+
+      ProjectPointOnPlane(level_forward, forward, refNormal);
+      ProjectPointOnPlane(level_right, right, refNormal);
+      VectorNormalize(level_forward);
+      VectorNormalize(level_right);
+      ProjectPointOnPlane(projected_forward, level_forward, pm->ps->grapplePoint);
+      ProjectPointOnPlane(projected_right, level_right, pm->ps->grapplePoint);
+      projected_forward_dist = Distance(level_forward, projected_forward);
+      if(projected_forward[2] < 0) {
+        projected_forward_dist = -projected_forward_dist;
+      }
+      projected_right_dist = Distance(level_right, projected_right);
+      if(projected_right[2] < 0) {
+        projected_right_dist = -projected_right_dist;
+      }
+      if(fabs(projected_right_dist + projected_forward_dist) > 0.5) {
+        angle_correction *=
+          projected_forward_dist /
+          (projected_right_dist + projected_forward_dist);
+      } else {
+        if(projected_forward_dist == 0.0f) {
+          angle_correction = 0;
+        } else if(projected_forward_dist < 0.0f) {
+          if(projected_right_dist + projected_forward_dist < 0.0f) {
+            angle_correction = 270;
+          } else {
+            angle_correction = 90;
+          }
+        } else {
+          if(projected_right_dist + projected_forward_dist < 0.0f) {
+            angle_correction = 90;
+          } else {
+            angle_correction = 270;
+          }
+        }
+      }
+    }
+
+    angle_correction = AngleNormalize360(angle_correction);
+
+    RotatePointAroundVector( rotated, pm->ps->grapplePoint, forward, angle_correction );
+    vectoangles( rotated, angles );
+
+    pm->ps->delta_angles[ YAW ] -= ANGLE2SHORT( angles[ YAW ] - pm->ps->viewangles[ YAW ] );
+  }
+}
 
 /*
 =============
@@ -1958,19 +2021,12 @@ static void PM_GroundClimbTrace( void )
     pm->ps->eFlags &= ~EF_WALLCLIMB;
 
     //just transided from ceiling to floor... apply delta correction
-    if( ( pm->ps->stats[ STAT_STATE ] & SS_WALLCLIMBINGCEILING ) || pm->ps->grapplePoint[2] < 0 )
-    {
-      vec3_t  forward, rotated, angles;
-
-      AngleVectors( pm->ps->viewangles, forward, NULL, NULL );
-
-      RotatePointAroundVector( rotated, pm->ps->grapplePoint, forward, 180.0f );
-      vectoangles( rotated, angles );
-
-      pm->ps->delta_angles[ YAW ] -= ANGLE2SHORT( angles[ YAW ] - pm->ps->viewangles[ YAW ] );
-    }
+    PM_Wall_Climb_Upside_Down_Drop( );
 
     pm->ps->stats[ STAT_STATE ] &= ~SS_WALLCLIMBINGCEILING;
+
+    //adjust ROLL
+    pm->ps->delta_angles[ ROLL ] -= ANGLE2SHORT( pm->ps->viewangles[ ROLL ] );
 
     //we get very bizarre effects if we don't do this :0
     VectorCopy( refNormal, pm->ps->grapplePoint );
@@ -2043,17 +2099,10 @@ static void PM_GroundTrace( void )
     }
 
     //just transided from ceiling to floor... apply delta correction
-    if( ( pm->ps->stats[ STAT_STATE ] & SS_WALLCLIMBINGCEILING ) || pm->ps->grapplePoint[2] < 0 )
-    {
-      vec3_t  forward, rotated, angles;
+    PM_Wall_Climb_Upside_Down_Drop( );
 
-      AngleVectors( pm->ps->viewangles, forward, NULL, NULL );
-
-      RotatePointAroundVector( rotated, pm->ps->grapplePoint, forward, 180.0f );
-      vectoangles( rotated, angles );
-
-      pm->ps->delta_angles[ YAW ] -= ANGLE2SHORT( angles[ YAW ] - pm->ps->viewangles[ YAW ] );
-    }
+    //adjust ROLL
+    pm->ps->delta_angles[ ROLL ] -= ANGLE2SHORT( pm->ps->viewangles[ ROLL ] );
   }
 
   pm->ps->stats[ STAT_STATE ] &= ~SS_WALLCLIMBING;
